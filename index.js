c15c1824365d26102cabe9ce9c23676db3909b4b
wissem19 <wissemlallahem@gmail.com>
2018-09-10 16:16:12 +0200

    test Majeur ou mineur

--- index.js
+++ index.js
@@ -12,6 +12,9 @@ app.post('/', function (req, res){
   var matches = req.body.Pwd.match(/\d+/g);
   var response = "";
   var testmail = req.body.mail;
+  var pseudoname = req.body.name;
+  var temp = new Date(req.body.datenaissance);
+  var tempsnow = new Date();
 
    if((req.body.Pwd.length < 8 ) )
    {
@@ -48,17 +51,35 @@ var char=req.body.Pwd.split('')
  
   var reg = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   if (reg.test(testmail)){
-    chaine1 ="test valide"
+    chaine1 ="test  mail valide"
 }else{
   chaine1 = "test non valide"
 }
-  res.send( chaine +" "+chaine1);
 
+for(var k = 0; k < pseudoname.length;k++){
+   if (pseudoname.indexOf(" ") == -1){
+     chaine2 ="contient pas espace "
+   }else{
+     chaine2="contient espace "
+   }
 
-   
+}
+console.log(temp);
+console.log(tempsnow)
+diff = tempsnow - temp
+console.log(diff)
+tmp = diff /(365*3600*24)
+console.log(tmp)
+tmp = Math.round(tmp)/1000
+tmp = Math.round(tmp)
+if(tmp > 18){
+  chaine3 = "Majeur"
+}else{
+  chaine3 = "Mineur"
+}
+
+  res.send( chaine +"    "+chaine1+"    "+chaine2 +"   "+chaine3+" avec age"+tmp);
 })
-
-
 app.listen(4500, function () {
   console.log('Example app listening on port 4500!')
-})  
+})  ;
