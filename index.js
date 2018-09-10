b5c8d6a0843c3120c03cf7eea9038fabe15ea70b
wissem19 <wissemlallahem@gmail.com>
2018-09-10 12:40:25 +0200

    test reg validee

--- index.js
+++ index.js
@@ -45,33 +45,7 @@ var char=req.body.Pwd.split('')
     break;
   }}
 
-  /*var maill = req.body.mail;  
-  var msg = "Votre Identifiant est invalide";
-  var nombre = 0;
-  var validion = 0;
-  var chaine1 = "" ;
-
-  for (var i = 0; i < maill.length; i++) {
-
-    if (maill[i] == "@" && i>1){
-        nombre = i;
-        validion = validion + 1;
-        msg = "@ exists"
-        chaine1 = msg;
-    }
-    if (maill[i] == "." && i>nombre+3){
-        nombre = i;
-        validion = validion + 1;
-        msg = "'.' exist"
-        chaine1 = msg;
-    }  
-    if (validion == 2 && i>nombre+2){
-        msg = "Votre Identifiant est valider";
-        chaine1 = msg ;
-    }
-      res.send(chaine+" "+chaine1);
-
-}*/  
+ 
   var reg = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   if (reg.test(testmail)){
     chaine1 ="test valide"
@@ -81,21 +55,9 @@ var char=req.body.Pwd.split('')
   res.send( chaine +" "+chaine1);
 
 
-   //if(req.body.Pwd.substr(0,1).toUpperCase()!=req.body.Pwd.substr(0,1))
-   //res.send(response)
-
+   
 })
-//app.post('/', function (req, res) {
-    //console.log(req.body)
-    //req.body.timestamp= Math.floor(new Date().getTime()/1000)
- // res.send(req.body)
-//})
-/*app.post('/', function (req, res) {
-  var maill = req.body.mail
-  console.log(maill);
-    res.send(maill)
-  })
-*/
+
 
 app.listen(4500, function () {
   console.log('Example app listening on port 4500!')
